7880e86a5d580f7288d20353bcecc1a96b0b7783
Ishan Prasad <ishanprasad.sahota@gmail.com>
2021-07-14 16:29:53 +0530

    upload.pl

--- scripts/upload.pl
+++ scripts/upload.pl
@@ -11,16 +11,19 @@ my $file = $query->param('image');
 my $dir = "/home/stud1034/apacheSSL/cgi-bin/photos";
 
 $file=~m/^.*(\\|\/)(.*)/;
- # strip the remote path and keep the filename
+ 
  my $name = $2;
 
 open(LOCAL, ">$dir/$name") or print "error";
 my $file_handle = $query->upload('image');
 
-while(<$file_handle>) {               // use that handle
+while(<$file_handle>) {               
     print LOCAL $_;
- }
- close($file_handle);                        // clean the mess
- close(LOCAL);                               // 
+}
+
+
+
+ close($file_handle);                        
+ close(LOCAL);                    
  print $q->header();
  print "<p> File upload successful. Go <a href='/grid.html'> back </a> to grid.";
