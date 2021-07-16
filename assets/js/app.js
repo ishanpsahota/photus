9192af0522ccd5e2f00a9cc7d0abf1c2cce1e234
Jad Altahan <xviyy@aol.com>
2021-07-16 03:18:35 -0700

    Improve handling of header buttons

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,21 +1,11 @@
 // Removes the text from the navbar buttons at the targeted screen width
 function hideNavbarButtonText(pageWidth) {
-  var eBtnGrid = document.querySelectorAll(".navbar-nav .btn-outline-dark")[0];
-  if (eBtnGrid != null) {
-    var icon = "<i class='fa fa-th' aria-hidden='true'></i>";
+  var navbarButtons = document.querySelectorAll(".nav-item button");
+  for (let i = 0; i < navbarButtons.length; i++) {
     if (pageWidth.matches)
-      eBtnGrid.innerHTML = icon;
+      navbarButtons[i].firstElementChild.style.display = "none";
     else
-      eBtnGrid.innerHTML = "Grid &nbsp;" + icon;
-  }
-
-  var eBtnUpload = document.querySelectorAll(".navbar-nav .btn-outline-primary")[0];
-  if (eBtnUpload != null) {
-    var icon = "<i class='fa fa-upload' aria-hidden='true'></i>";
-    if (pageWidth.matches)
-      eBtnUpload.innerHTML = icon;
-    else
-      eBtnUpload.innerHTML = "Upload &nbsp;" + icon;
+      navbarButtons[i].firstElementChild.style.display = "inline";
   }
 }
 
