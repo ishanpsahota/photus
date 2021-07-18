10812a7b7745980ddd7231c028dafe214eada442
Jad Altahan <xviyy@aol.com>
2021-07-17 21:21:31 -0700

    Redesign upload form

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,19 @@
+const TEMPLATE_FILE_UPLOAD_LIST_ITEM = `
+  <div class="file-info">
+    <div class="file-name">
+      <strong>File:</strong>
+    </div>
+  </div>
+  <button class="file-action" title="Upload">
+    <i class="fa fa-upload" aria-hidden="true"></i>
+  </button>
+  <button class="file-action" title="Remove" type="reset" onclick="uploader.resetList(true)">
+    <i class="fa fa-trash-alt" aria-hidden="true"></i>
+  </button>`;
+
 // Removes the text from the navbar buttons at the targeted screen width
 function hideNavbarButtonText(pageWidth) {
-  var navbarButtons = document.querySelectorAll(".nav-item button");
+  let navbarButtons = document.querySelectorAll(".nav-item button");
   for (let i = 0; i < navbarButtons.length; i++) {
     if (pageWidth.matches)
       navbarButtons[i].firstElementChild.style.display = "none";
@@ -12,3 +25,75 @@ function hideNavbarButtonText(pageWidth) {
 var pageWidthQuery = window.matchMedia("(max-width: 500px)");
 hideNavbarButtonText(pageWidthQuery);
 pageWidthQuery.addListener(hideNavbarButtonText);
+
+
+class ImageUploader {
+  constructor(uploadForm) {
+    this.form = document.querySelector(uploadForm);
+    this.dropzone = this.form.querySelectorAll(".dropzone, .dropzone-icon");
+    this.list = this.form.querySelector('.file-display');
+    this.input = this.form.querySelector('#file-input');
+
+    this.updateDropzoneStyle = this.updateDropzoneStyle.bind(this);
+    this.addFile = this.addFile.bind(this);
+    this.resetList = this.resetList.bind(this);
+
+    this.eventListen();
+  }
+
+  eventListen() {
+    this.dropzone[0].addEventListener("dragover", this.updateDropzoneStyle);
+    this.dropzone[0].addEventListener("dragleave", this.updateDropzoneStyle);
+    this.dropzone[0].addEventListener("drop", this.updateDropzoneStyle);
+
+    this.input.addEventListener("change", this.addFile);
+  }
+
+  updateDropzoneStyle(e) {
+    const action = (e.type === "dragover" ? "add" : "remove");
+    e.currentTarget.classList[action]("active");
+    this.dropzone[1].classList[action]("active");
+  }
+
+  addFile(e) {
+    const files = e.currentTarget.files;
+
+    // In case the user presses "Cancel" in the browser dialog
+    if (files.length == 0)
+      return;
+
+    if (this.form.children.length > 0)
+      this.resetList(false);
+
+    // SECURITY WARNING: THIS CAN BE SPOOFED BY EXT CHANGE
+    if (!/^image\/jpeg|jpg|png/.test(files[0].type)) {
+      this.form.reset();
+      return;
+    }
+
+    this.addToList(files[0]);
+  }
+
+  addToList(file) {
+    const li = document.createElement("li");
+    li.classList.add("file");
+    li.innerHTML = TEMPLATE_FILE_UPLOAD_LIST_ITEM;
+
+    const fileName = li.querySelector(".file-name");
+    fileName.childNodes[2].textContent = file.name;
+
+    this.list.appendChild(li);
+    this.list.style.display = "block";
+    return li;
+  }
+
+  resetList(includeForm) {
+    includeForm = (typeof includeForm !== 'undefined') ?  includeForm : false
+
+    this.list.innerHTML = "";
+    this.list.style.display = "none";
+
+    if (includeForm)
+      this.form.reset();
+  }
+}
