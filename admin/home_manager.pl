d39ce91e2c4735a4155f4ac13faa0d46f792245c
Jad Altahan <xviyy@aol.com>
2021-07-24 13:45:21 -0700

    Rework Upload Manager page

--- admin/home_manager.pl
+++ admin/home_manager.pl
@@ -9,25 +9,21 @@ my $index = 0;
 
 open(my $fh, '<', $photos_file) or die "Could not find any photos $!";
 
-
 while(my $p = <$fh>) {  
-    print '<DIV class="card card-grid-item">';
-    print '<IMG class="card-img-top" src="';
-    print $p;
-    print '" alt="image"/>';
-    print '<DIV class="card-body text-center">';
-    print '<FORM action="/cgi-bin/approve.cgi" method="POST">';
-    print '<DIV class="form-group">';
-    print '<DIV class="form-check form-check-inline">';
-    print '<LABEL class="form-check-label m-1 btn btn-primary">';
-    print '<INPUT class="form-check-input" type="radio" name="allow" id="allow_radio_true" value="true">';
-    print '<SPAN class=""> Allow <I class="fa fa-check" aria-hidden="true"></I> </SPAN> </LABEL>';
-    print '<LABEL class="form-check-label m-1 btn btn-outline-danger">';
-    print '<INPUT class="form-check-input" type="radio" name="allow" id="allow_radio_false" value="false"> ';
-    print '<SPAN class="">Reject <I class="fa fa-times" aria-hidden="true"></I></SPAN> </LABEL>';
-    print '<INPUT type="hidden" name="image" value="';
-    print $p;
-    print '" /> </DIV> </DIV>';
-    print '<DIV class="form-group"> <BUTTON type="submit" class="btn btn-dark">Submit</BUTTON> </DIV>';
-    print '</FORM> </DIV></DIV>';    
+print <<CARD_HTML;
+<li class="upload-manager-card">
+  <div class="upload-manager-card-image" style="background-image: url($p)"></div>
+  <form action="/cgi-bin/approve.cgi" method="POST">
+    <div class="upload-manager-switch">
+      <input type="radio" id="radio-approve-$." name="allow" value="true" checked>
+      <label for="radio-approve-$.">Approve<i class="fa fa-check" aria-hidden="true"></i></label>
+      
+      <input type="radio" id="radio-reject-$." name="allow" value="false">
+      <label for="radio-reject-$.">Reject<i class="fa fa-times" aria-hidden="true"></i></label>
+    </div>
+    <input type="hidden" name="image" value="$p">
+    <button type="submit" class="upload-manager-button">Confirm Decision</button>
+  </form>
+</li>
+CARD_HTML
 }
