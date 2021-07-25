45279efccb37424cc3142826a70b2c8d07f1b617
Ishan Prasad <ishanprasad.sahota@gmail.com>
2021-07-25 17:33:54 +0530

    length check failed. correcting errors.

--- admin/grid-manager.pl
+++ admin/grid-manager.pl
@@ -9,34 +9,31 @@ print "Content-type: text/html\n\n";
 my $photos_file = "/home/stud1034/apacheSSL/cgi-bin/pending.txt";
 my $index = 0;
 
-my $count = `wc -l < $photos_file`
-die "wc failed: $?" if $?;
+open(my $fh, '<', $photos_file) or die "Could not find any photos $!";
+
+my $count = `wc -l < $photos_file`;
 chomp($count);
-
 if($count eq 0) {
-  print "No one has uploaded anything yet. Check back later!";
+  die "Cannot get any images. Check back later.\n";
 }
-else {
-  open(my $fh, '<', $photos_file) or die "Could not find any photos $!";
 
-  while(my $p = <$fh>) {  
-    print <<CARD_HTML;
-    <li class="upload-manager-card">
-      <div class="upload-manager-card-image" style="background-image: url($p)"></div>
-      <form action="/cgi-bin/approve.cgi" method="POST">
-        <div class="upload-manager-switch">
-          <input type="radio" id="radio-approve-$." name="allow" value="true" checked>
-          <label for="radio-approve-$.">Approve<i class="fa fa-check" aria-hidden="true"></i></label>
-          
-          <input type="radio" id="radio-reject-$." name="allow" value="false">
-          <label for="radio-reject-$.">Reject<i class="fa fa-times" aria-hidden="true"></i></label>
-        </div>
-        <input type="hidden" name="image" value="$p">
-        <button type="submit" class="upload-manager-button">Confirm Decision</button>
-      </form>
-    </li>    
-    CARD_HTML
-
-  }
+while(my $p = <$fh>) {  
+print <<CARD_HTML;
+<li class="upload-manager-card">
+  <div class="upload-manager-card-image" style="background-image: url($p)"></div>
+  <form action="/cgi-bin/approve.cgi" method="POST">
+    <div class="upload-manager-switch">
+      <input type="radio" id="radio-approve-$." name="allow" value="true" checked>
+      <label for="radio-approve-$.">Approve<i class="fa fa-check" aria-hidden="true"></i></label>
+      
+      <input type="radio" id="radio-reject-$." name="allow" value="false">
+      <label for="radio-reject-$.">Reject<i class="fa fa-times" aria-hidden="true"></i></label>
+    </div>
+    <input type="hidden" name="image" value="$p">
+    <button type="submit" class="upload-manager-button">Confirm Decision</button>
+  </form>
+</li>
+CARD_HTML
 }
+
 
